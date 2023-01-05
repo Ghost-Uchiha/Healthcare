844bbd67ed7c1cca7b8a17eea0aec76908cfac92
Ghost-Uchiha <120158935+Ghost-Uchiha@users.noreply.github.com>
2023-01-05 16:39:34 +0530

    Add files via upload

---
+++ Hospital/connection.php
@@ -0,0 +1,7 @@
+<?php
+$db = mysqli_connect('localhost','root','','online_health_care');
+if (!$db){ 
+       echo "connection failed";
+}
+error_reporting(0);
+?>
---
+++ Hospital/index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Browse Hospital </title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/css/bootstrap.min.css" rel="stylesheet" >
+        <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" ></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.min.js" integrity="sha384-Atwg2Pkwv9vp0ygtn1JAojH0nYbwNJLPhwyoVbhoPwBhjQPR5VtM2+xf0Uwh9KtT" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+      </head>
+    <body style="background-image: url(Photos/healthcare-banner.jpg);">
+      
+      <header>
+        <div class="container-fluid">
+          <nav class="navbar navbar-expand-md navbar-dark bg-dark ">
+              <span class="navbar-text" style=" font-size: 30px;">Healthcare</span>
+  
+              <ul class="navbar-nav">
+                <li class="nav-item" style="padding: 0 25px; font-size: 30px;"> <a style="color: white;" href="index.php" class="nav-link"> Home</a> </li>
+                <li class="nav-item" style="padding: 0 25px; font-size: 30px;"> <a href="https://www.caringbridge.org/resources/motivational-quotes-for-patients/" class="nav-link"style="color: white;"> Quoets</a> </li>
+                <li class="nav-item" style="padding: 0 25px; font-size: 30px;"> <a href="payment.html" class="nav-link"style="color: white;"> Payment</a> </li>
+                <li class="nav-item" style="padding: 0 25px; font-size: 30px;"> <a href="login.php" class="nav-link"style="color: white;"> LogIn</a> </li>
+  
+              </ul>
+  
+              <form class="d-flex" role="search">
+                <li class="nav-item dropdown">
+                  <a  data-toggle="dropdown" href="#location" style="color: white; font-size: 30px;">Hospital Location  <span class="caret"></span></a>
+                  <ul class="dropdown-menu"> 
+               <li> <a href="location map/dhaka.html">Dhaka</a></li>
+              <li>  <a href='location map/chittagong.html'>Chittagong</a></li>
+               <li> <a href="location map/barishal.html">Barishal</a></li>
+              <li>  <a href="location map/khulna.html">Khulna</a></li>
+             <li>   <a href='location map/mymensingh.html'>Mymensingh</a></li>
+             <li>   <a href='location map/rajshahi.html'>Rajshahi</a></li>
+             <li>   <a href='location map/rangpur.html'>Rangpur</a></li>
+             <li>   <a href='location map/sylhet.html'>Sylhet</a></li>
+                </ul>
+                </li>          
+                  </form>
+            </nav>
+        </div>
+    </header>
+
+    
+            
+              </div>
+            </div>
+          </nav>
+    </body>
+</html>
